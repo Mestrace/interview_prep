b14c4d260f9d7c9831dbc2103e8a0d10fab43213
Yida Liu <mestrace@Yida-MBP16.local>
2021-02-02 09:16:38 +0800

    cci/ch8_dp/p8_2 完成DFS版本

--- coding/cci/ch8_dp/p8_2/impl.go
+++ coding/cci/ch8_dp/p8_2/impl.go
@@ -4,6 +4,48 @@ package p8_2
 // 如果可以走, 返回一个列表
 // 如果不能走, 返回空
 
-func RobotInAGrid_DFS(grid [][]uint8, i, j uint) {
+func RobotInAGridDFS(grid [][]uint8) [][]int {
+	result := robotInAGridDFS(grid, 0, 0)
+	reverse(result)
+	return result
+}
 
+func robotInAGridDFS(grid [][]uint8, i, j int) [][]int {
+	if grid[j][i] > 0 {
+		return nil
+	}
+	if i == len(grid[0])-1 && j == len(grid)-1 {
+		result := make([][]int, 0, i+j)
+		result = append(result, []int{i, j})
+		return result
+	} else if i == len(grid[0])-1 {
+		if result := robotInAGridDFS(grid, i, j+1); result != nil {
+			return append(result, []int{i, j})
+		}
+	} else if j == len(grid)-1 {
+		if result := robotInAGridDFS(grid, i+1, j); result != nil {
+			return append(result, []int{i, j})
+		}
+	} else {
+		if result := robotInAGridDFS(grid, i+1, j); result != nil {
+			return append(result, []int{i, j})
+		}
+		if result := robotInAGridDFS(grid, i, j+1); result != nil {
+			return append(result, []int{i, j})
+		}
+	}
+
+	return nil
+}
+
+func reverse(path [][]int) {
+	for i, tmp := range path {
+		if i >= len(path)/2 {
+			break
+		}
+		end := len(path) - i - 1
+		println(i, end)
+		path[i] = path[end]
+		path[end] = tmp
+	}
 }
--- coding/cci/ch8_dp/p8_2/impl_test.go
+++ coding/cci/ch8_dp/p8_2/impl_test.go
@@ -1,16 +1,54 @@
 package p8_2_test
 
 import (
+	impl "github.com/mestrace/interview_prep/coding/cci/ch8_dp/p8_2"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	impl "github.com/mestrace/interview_prep/coding/cci/ch8_dp/p8_2"
 )
 
 var _ = Describe("Impl", func() {
 	Context("DFS", func() {
 		It("should work", func() {
-			impl.RobotInAGrid_DFS()
+			grid := [][]uint8{
+				{0, 0, 0},
+				{0, 0, 0},
+			}
+
+			path := impl.RobotInAGridDFS(grid)
+
+			Expect(path).To(Equal([][]int{
+				{0, 0},
+				{1, 0},
+				{2, 0},
+				{2, 1},
+			}))
+		})
+
+		It("should work with obstacle", func() {
+			grid := [][]uint8{
+				{0, 0, 1},
+				{0, 0, 0},
+			}
+
+			path := impl.RobotInAGridDFS(grid)
+
+			Expect(path).To(Equal([][]int{
+				{0, 0},
+				{1, 0},
+				{1, 1},
+				{2, 1},
+			}))
+		})
+
+		It("should return nil with no obstacle", func() {
+			grid := [][]uint8{
+				{0, 0, 1},
+				{0, 1, 0},
+			}
+
+			path := impl.RobotInAGridDFS(grid)
+
+			Expect(path).To(Equal([][]int(nil)))
 		})
 	})
 })
@@ -23,4 +61,8 @@ func Grid(i, j uint) (grid [][]uint8) {
 	}
 
 	return
+}
+
+func PlaceObstacles(grid [][]uint8, i, j uint) {
+	grid[j][i] = 1
 }
