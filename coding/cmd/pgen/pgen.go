ecd4012bbab6f7493421796cf909918e639e1e52
Yida Liu <yida.liu@qq.com>
2021-02-08 23:56:13 +0800

    pgen fix output

--- coding/cmd/pgen/pgen.go
+++ coding/cmd/pgen/pgen.go
@@ -12,11 +12,13 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-type ImplTplData struct {
+type implTplData struct {
 	Pkg string
 }
 
 func main() {
+	log.SetOutput(os.Stdout)
+
 	app := cli.NewApp()
 	app.Name = "pgen"
 	app.Usage = "Generates code for interview prep"
@@ -38,51 +40,68 @@ func main() {
 
 		path := filepath.Join(".", pkg)
 
-		// crate path
+		// crate path if not exist
 		syscall.Umask(0)
 		if _, err := os.Stat(path); os.IsNotExist(err) {
 			os.Mkdir(path, 0700)
 		} else if err != nil {
+			log.Fatalf("Failed to create path because %s\n", err.Error())
 			return err
 		}
 
 		// create `impl.go`
 		impl, err := os.Create(filepath.Join(path, "impl.go"))
 		if err != nil {
+			log.Fatalf(failedToCreateFileStr, "impl.go")
 			return err
 		}
+		defer impl.Close()
 
 		// open template
 		tpl, err := template.New("pgen").Parse(implTemplate)
 		if err != nil {
+			log.Fatalf("Failed to parse template because %s\n", err.Error())
 			return err
 		}
 
 		// generate file
-		if err := tpl.Execute(impl, ImplTplData{
+		if err := tpl.Execute(impl, implTplData{
 			Pkg: pkg,
 		}); err != nil {
+			log.Fatalf("Failed to generate template file because %s\n", err.Error())
 			return err
 		}
 
 		// > ginkgo bootstrap
 		cmd := exec.Command("ginkgo", "bootstrap")
 		cmd.Dir = path
-		if err := cmd.Run(); err != nil {
+		cmd.Dir = path
+		sout, err := cmd.Output()
+		fmt.Print(string(sout))
+		if err != nil {
+			log.Fatalf(ginkgoFailedErrStr, err.Error())
 			return err
 		}
 
 		// > ginkgo generate impl.go
 		cmd = exec.Command("ginkgo", "generate", "impl.go")
 		cmd.Dir = path
-		if err := cmd.Run(); err != nil {
+		sout, err = cmd.Output()
+		fmt.Print(string(sout))
+		if err != nil {
+			log.Fatalf("Unable to generate test for %s, because %s\n", "impl.go", err.Error())
 			return err
 		}
 
 		return nil
 	}
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatalln(err)
-	}
+
+	app.Run(os.Args)
 }
+
+const (
+	ginkgoFailedErrStr = `Execute ginkgo command failed because %s
+Ginkgo might not have been installed.
+Try run "go get -u github.com/onsi/ginkgo/ginkgo"`
+	failedToCreateFileStr = `Failed to create file %s\n`
+)
